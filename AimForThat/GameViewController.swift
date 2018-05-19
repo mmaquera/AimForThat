091b81af19995da58d55e69475e2cf9b0fe0c1bf
Boris <marco.arka@gmail.com>
2018-05-19 12:44:17 -0500

    Creacion del proyecto y agregado de mi primer botón

---
+++ AimForThat/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AimForThat
+//
+//  Created by Marco Maquera on 5/19/18.
+//  Copyright © 2018 Marco Boris. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        print("Hey pulsado el botón")
+    }
+    
+}
+
